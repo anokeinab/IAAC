0a9dfb6ed4c3499a8bade96c51d13eb737b1b78d
anokeinab <divinbaniekona@gmail.com>
2025-12-12 18:06:32 -0600

    remove variables file from gitignore

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for Amazon Linux 2023"
+  default     = "ami-0199d4b5b8b4fde0e"
+}
+
+variable "key_name" {
+  description = "Name of the existing EC2 Key Pair"
+  type        = string
+  default     = "hp-envy-home"
+}
+
+variable "ami_ubuntu_id" {
+  description = "AMI ID for Ubuntu 24"
+  default     = "ami-0cfde0ea8edd312d4"
+}
